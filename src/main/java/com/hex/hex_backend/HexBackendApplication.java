c7f949cfbf711f1e8d8cc193d7aac16d055c54f4
Alan Acevedo <acevedo.j.alan@gmail.com>
2026-07-25 21:08:37 -0300

    fix: timezone arg

--- src/main/java/com/hex/hex_backend/HexBackendApplication.java
+++ src/main/java/com/hex/hex_backend/HexBackendApplication.java
@@ -1,5 +1,7 @@
 package com.hex.hex_backend;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +9,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HexBackendApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+		
 		SpringApplication.run(HexBackendApplication.class, args);
 	}
 
